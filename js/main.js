912145e5c0c09922849d5ffd76ec1a137e18f0fa
Kirill Dmitriev <bulletformyvalentine67.ru@mail.ru>
2016-10-10 19:47:58 +0300

    Add java-script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,107 @@
+// (function(){
 
+	//Varuables
+// 	var increaseRad = $('#increase-radius'),
+// 		reduseRad = $('#reduce-radius'),
+// 		create = $('.create'),
+// 		maxRadius = 20,
+// 		minRadius = 0;
+
+// 		increaseRad.on('click', function() {
+
+// 			var currentRadius = create.css('border-radius'),
+// 				step = $('#step').val(),
+// 				newRadius = (parseInt(currentRadius) + parseInt(step));
+
+// 				if(newRadius > maxRadius){
+// 					newRadius = maxRadius;
+// 					$(this).addClass('disabled');
+// 				}
+// 				if(newRadius > minRadius){
+// 					reduseRad.removeClass('disabled');
+// 				}
+
+// 				create.css({
+// 					'border-radius' : newRadius
+// 				})
+// 				console.log(currentRadius);
+// 				console.log(step);
+
+// 		});
+
+// 		reduseRad.on('click', function() {
+
+// 			var currentRadius = create.css('border-radius'),
+// 				step = $('#step').val(),
+// 				newRadius = (parseInt(currentRadius) - parseInt(step));
+
+// 				if(newRadius < minRadius){
+// 					newRadius = minRadius;
+// 					$(this).addClass('disabled');
+// 				}
+// 				if(newRadius < maxRadius){
+// 					increaseRad.removeClass('disabled');
+// 				}
+
+// 				create.css({
+// 					'border-radius' : newRadius
+// 				})
+// 				console.log(currentRadius);
+// 				console.log(step);
+
+// 		});
+
+
+// }());
+
+(function(){
+
+	var increaseRad = $('#increase-radius'),
+		reduceRad = $('#reduce-radius'),
+		create = $('.create'),
+		maxRadius = 20,
+		minRadius = 0;
+
+		increaseRad.on('click', function(){
+			var currentRadius = create.css('border-radius'),
+				step = $('#step').val(),
+				newRadius = (parseInt(currentRadius) + parseInt(step));
+
+				if(newRadius > maxRadius){
+					newRadius = maxRadius,
+					$(this).addClass('disabled')
+				}
+
+				if(newRadius > minRadius){
+					reduceRad.removeClass('disabled');
+				}
+
+				create.css({
+					'border-radius' : newRadius
+				})
+				console.log(currentRadius);
+				console.log(step);
+
+		})
+
+		reduceRad.on('click', function(){
+			var currentRadius = create.css('border-radius'),
+				step = $('#step').val(),
+				newRadius = (parseInt(currentRadius) - parseInt(step));
+
+				if(newRadius < minRadius){
+					newRadius = maxRadius,
+					$(this).addClass('disabled')
+				}
+
+				if(newRadius < maxRadius){
+					increaseRad.removeClass('disabled');
+				}
+
+				create.css({
+					'border-radius' : newRadius
+				})
+				console.log(currentRadius);
+				console.log(step);
+		})
+}());
